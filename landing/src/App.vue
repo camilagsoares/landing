4d7034be47034d237e55f645871a140abb4d22ba
camilagsoares <camila.soares19@hotmail.com>
2022-04-14 23:20:24 -0300

    Starting the project

---
+++ landing/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <nav-bar />
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import NavBar from './components/NavBar.vue'
+import './assets/global.css'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+    NavBar
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ landing/src/components/NavBar.vue
@@ -0,0 +1,13 @@
+<template>
+    <h2>Menu...</h2>
+</template>
+
+
+<script></script>
+
+
+<style scoped>
+h2{
+    color: red;
+}
+</style>
